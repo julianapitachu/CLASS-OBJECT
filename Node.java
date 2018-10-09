aac9807e0f79666f5e2a0519c6c4245b58b7acda
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-09 07:54:11 -0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,29 @@
+
+public class Node {
+	
+	// Attributes 
+	String Label;
+	int Value;
+	
+	// Method getter
+	public String getLabel()
+	{
+		return Label;
+	}
+	public int getValue()
+	{
+		return Value;
+	}
+	
+	// Method setter
+	void setLabel(String Label)
+	{
+		this.Label = Label;
+	}
+	void setValue(int Value)
+	{
+		this.Value = Value;
+	}
+	
+	
+}
