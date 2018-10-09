d033ffd713f6841cbfeb84b4a52b571a0c579404
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-09 07:09:47 -0700

    Update and rename README.md to Kotak.java

---
+++ Kotak.java
@@ -0,0 +1,55 @@
+# CLASS-OBJECT
+
+public class Kotak {
+	
+	// Attributes 
+	private double panjang, lebar, Luas, keliling;
+	double setpanjang;
+	double setlebar;
+	
+	// Method getter
+	public double getpanjang()
+	{
+		return panjang; 
+	}
+    public double getlebar()
+    {
+    	return lebar;
+    }
+    public double getLuas()
+    {
+    	return Luas;
+    }
+    public double getkeliling()
+    {
+    	return keliling;
+    }
+    
+    // Method setter
+    public void setpanjang(double panjang)
+    {
+    	this.panjang = panjang;
+    }
+    public void setlebar(double lebar)
+    {
+    	this.lebar = lebar;
+    }
+	public void setLuas(double Luas) 
+	{
+		this.Luas = Luas;
+	}
+	public void setkeliling(double keliling)
+	{
+		this.keliling = keliling;
+	}
+	
+	// Method
+	void hitungLuas(){
+		Luas = panjang * lebar;
+	}
+	void hitungkeliling(){
+		keliling = panjang + lebar + panjang + lebar;
+	}
+				
+
+}
