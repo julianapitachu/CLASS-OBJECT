ba1737ff837e2e904eabcc249d54268cd10b832f
julianapitachu <43759141+julianapitachu@users.noreply.github.com>
2018-10-09 09:29:05 -0700

    Update and rename Node.java to Stack.java

---
+++ Stack.java
@@ -0,0 +1,13 @@
+
+public class Stack {
+	
+	// Attributes 
+		String[] value = new String[100];
+		int Top;
+		
+    // Method setter
+		String setvalueAt(int position, String value){
+			return value;
+
+		}
+}
